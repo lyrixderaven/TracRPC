bd41eb29f7db1bfdd36db71a4a3402104d00d3d1
Florian Cech <loki@fsinf.at>
2015-06-27 10:21:03 +0200

    Added sublime-workspace to gitignore Refactored the TracListTickets classes for better extendability

--- TracListTickets.py
+++ TracListTickets.py
@@ -1,6 +1,8 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import webbrowser
 import xmlrpc.client
+
 
 class Settings():
 
@@ -12,11 +14,62 @@ class Settings():
         else:
             self.proj = args
 
-
     def get(self, key, default):
         return self.proj.get(key, self.user.get(key, default))
 
-class TracListTicketsCommand(sublime_plugin.TextCommand):
+
+class TracController():
+
+    def __init__(self, view, trac_url=None, trac_user=None, trac_pw=None):
+        self.view = view
+        self.base_url = trac_url or self.view.settings().get("trac_url")
+        self.rpc_url = "{}/login/rpc".format(self.base_url)
+
+        self.user = trac_user or self.view.settings().get("trac_user")
+        self.pw = trac_pw or self.view.settings().get("trac_pw")
+
+        self.rpc_url = self.rpc_url.replace('https://', 'https://{}:{}@'.format(
+            self.user,
+            self.pw))
+
+    def _get_tickets(self, query):
+
+        trac_server = xmlrpc.client.ServerProxy(self.rpc_url)
+        ticket_ids = trac_server.ticket.query(query)
+
+        multicall = xmlrpc.client.MultiCall(trac_server)
+
+        for ticket_id in ticket_ids:
+            multicall.ticket.get(ticket_id)
+
+        return multicall()
+
+    def get_active_user_tickets(self):
+        qstr = "owner=~{}&status!=closed".format(self.user)
+        #qstr = "owner=~{}&order=id".format(self.user)
+
+        tickets_response = self._get_tickets(qstr)
+        tickets = []
+        for ticket in tickets_response:
+            ticket_string = "#{} {}".format(ticket[0], ticket[3]['summary'])
+            ticket_url = "{}/ticket/{}".format(self.base_url, ticket[0])
+            tickets.append({
+                'id': ticket[0],
+                'title_string': ticket_string,
+                'url': ticket_url
+            })
+
+        return tickets
+
+
+class BaseTracCommand(sublime_plugin.TextCommand):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseTracCommand, self).__init__(*args, **kwargs)
+        self.controller = TracController(self.view)
+
+
+class TracListTicketsCommand(BaseTracCommand):
 
     def on_done(self, index):
 
@@ -26,49 +79,13 @@ class TracListTicketsCommand(sublime_plugin.TextCommand):
             return
 
         # if user picks from list, return the correct entry
-        url = self.tickets[index]['url']
+        url = self.active_tickets[index]['url']
         webbrowser.open_new_tab(url)
 
-    def collect_tickets(self):
-        qstr = "owner=~{}&status!=closed".format(self.user)
-        #qstr = "owner=~{}&order=id".format(self.user)
-        url = self.rpc_url.replace('https://','https://{}:{}@'.format(
-            self.user,
-            self.pw))
-        print(url)
-        trac_server = xmlrpc.client.ServerProxy(url)
-        ticket_ids = trac_server.ticket.query(qstr)
-
-        multicall = xmlrpc.client.MultiCall(trac_server)
-
-        for ticket_id in ticket_ids:
-            multicall.ticket.get(ticket_id)
-
-        tickets = multicall()
-
-        for ticket in tickets:
-            ticket_string = "#{} {}".format(ticket[0], ticket[3]['summary'])
-            ticket_url = "{}/ticket/{}".format(self.base_url, ticket[0])
-            self.tickets.append({
-                'id': ticket[0],
-                'title_string': ticket_string,
-                'url': ticket_url
-                })
-
-    def init(self):
-        self.base_url = self.view.settings().get("trac_url")
-        self.rpc_url = "{}/login/rpc".format(self.base_url)
-        self.user = self.view.settings().get("trac_user")
-        self.pw = self.view.settings().get("trac_pw")
-        self.auth = (self.user, self.pw)
-
-        self.tickets = []
-        self.collect_tickets()
-
     def run(self, edit, **args):
-        self.init()
 
-        ticket_list = [t['title_string'] for t in self.tickets]
+        self.active_tickets = self.controller.get_active_user_tickets()
+        ticket_list = [t['title_string'] for t in self.active_tickets]
 
         self.view.window().show_quick_panel(ticket_list, self.on_done, 1, 2)
 
@@ -81,7 +98,7 @@ class TracPasteReferenceCommand(TracListTicketsCommand):
         #  if canceled, index is returned as  -1
         if index == -1:
             return
-        ref_string = "[refs #{}]".format(self.tickets[index]['id'])
+        ref_string = "[refs #{}]".format(self.active_tickets[index]['id'])
         print(ref_string)
         print(self.view.sel()[0].begin())
 
@@ -89,4 +106,4 @@ class TracPasteReferenceCommand(TracListTicketsCommand):
         self.view.run_command(
             "insert",
             {'characters': ref_string}
-            )
+        )
